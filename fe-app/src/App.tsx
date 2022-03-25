a6ee1eb1ba5ade916b8c9b542496dba83582e377
Libikk <libikpl@gmail.com>
2022-03-25 09:59:51 +0000

    chore: add FE

---
+++ fe-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from './components/Layout';
+import './App.scss';
+
+const App:React.FC = () => {
+  return (
+    <div className='app'>
+      <Layout>
+        123
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
