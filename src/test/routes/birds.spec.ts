8dc5dc2ef452f60c0e0e68881becab2bb298f7bd
Libikk <libikpl@gmail.com>
2022-03-27 11:48:17 +0100

    test: improver test to check sort

--- src/test/routes/birds.spec.ts
+++ src/test/routes/birds.spec.ts
@@ -1,19 +1,27 @@
 import app from '../../app';
 import * as chai from 'chai';
-const chaiHttp = require('chai-http');
 const server = app.listen()
 
 chai.should();
-chai.use(chaiHttp);
+chai.use(require('chai-http'));
 
 describe('/birds endpoint', () => {
     it('should return sorted alphabetically birds /birds GET', (done) => {
         chai.request(server)
             .get('/birds')
             .end((err, res) => {
+                const sortedBirds = [...res.body].sort((a, b) => {
+                    const nameA = a.name.toLowerCase();
+                    const nameB = b.name.toLowerCase();
+                    if (nameA < nameB) return -1;
+                    if (nameA > nameB) return 1;
+                    return 0;
+                })
+
                 res.should.have.status(200);
                 res.should.be.json;
                 res.body.should.be.a('array');
+                chai.expect(sortedBirds).to.eql(res.body)
                 done();
             });
     });
